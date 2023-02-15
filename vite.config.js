d179b48f9bd9e4279f97014dfe07aee0b8c75188
Ezequiel Aleman <zekeale777@gmail.com>
2023-02-15 18:56:01 +1000

    images working on localhost

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: "/web_portfolio/",
+    publicDir: "/public",
     server: {
         proxy: {
             '/api': {
